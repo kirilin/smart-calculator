4fde91085d213630485102cd289e94c39cb28fe9
Yuri Kirilin <yura.kirilin@gmail.com>
2018-03-04 01:27:53 +0300

    Task: smart-calculator solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,95 @@
 class SmartCalculator {
+
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
+    this.operation = [];
   }
 
   add(number) {
-    // your implementation
+    this.operation.push({
+      name: 'add',
+      impl: (a, b) => {return a + b;},
+      order:  30,
+      num: number
+    });
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.operation.push({
+      name: 'subtract',
+      impl: (a, b) => {return a - b;},
+      order: 30,
+      num: number
+    });
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.operation.push({
+      name: 'multiply',
+      impl: (a, b) => {return a * b;},
+      order: 20,
+      num: number
+    });
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.operation.push({
+      name: 'devide',
+      impl: (a, b) => {return a / b;},
+      order: 20,
+      num: number
+    });
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.operation.push({
+      name: 'pow',
+      impl: (a, b) => {return Math.pow(a, b);},
+      order: 10,
+      num: number
+    });
+    return this;
+  }
+
+  get calcResult() {   
+    let revNotation = [this.initialValue];
+    let revOperation = [];
+    
+    this.operation.forEach(item => {     
+      if(item.name !== 'pow') {
+        while (revOperation.length &&
+            (revOperation[revOperation.length - 1].order <= item.order)) {
+          revNotation.push(revOperation.pop().impl); 
+        }
+      }
+      revOperation.push(item);
+      revNotation.push(item.num);
+    });
+
+    while (revOperation.length) {
+      revNotation.push(revOperation.pop().impl);
+    }
+    
+    let result = [];
+    let valueB;
+    revNotation.forEach(item => {
+      if (typeof(item) === 'number') {
+        result.push(item);
+      } else {
+        valueB = result.pop();
+        result.push(item(result.pop(), valueB));
+      }
+    });
+    
+    return result.pop();
+  }
+  
+  valueOf() {
+    return this.calcResult;
   }
 }
 
